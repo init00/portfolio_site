6c5bbc0a61f2d96cced9d8bbf18efc86c2388bd6
Abs Saurabh <abs.saurabh04@gmail.com>
2020-02-02 00:13:33 +0530

    Removed packages from package.json. Added CDN links.

--- src/html.js
+++ src/html.js
@@ -7,6 +7,7 @@ export default function HTML(props) {
       <head>
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossOrigin="anonymous"></link>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
         <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css"/>
         <meta
@@ -15,6 +16,9 @@ export default function HTML(props) {
         />
         {props.headComponents}
       </head>
+      <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossOrigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossOrigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossOrigin="anonymous"></script>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
         <div
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react"
-import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/site_style.css'
 import Navbar from '../components/UI/navigation/navbar/navbar'
 import Banner from '../components/UI/banner/banner'
